c31421db02b4d6472024bf106fe52e04421afeca
M. Afifudin <afif.x.udin@gmail.com>
2022-05-06 16:45:19 +0700

    chore: setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:unicorn/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-template-curly-in-string': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-null': 'off',
+  },
+};
