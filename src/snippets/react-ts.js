5bf77e76392784c5c2901af8927ed3e283d5791a
M. Afifudin <afif.x.udin@gmail.com>
2022-05-08 12:15:45 +0700

    feat: add react ts snippet v2

--- src/snippets/react-ts.js
+++ src/snippets/react-ts.js
@@ -67,3 +67,71 @@ exports.tsReactArrowFunctionExportComponent = {
   ],
   description: 'React Arrow Function Component',
 };
+
+exports.tsReactFunctionalComponentV2 = {
+  prefix: 'rfc $$rfc(ts)',
+  body: [
+    'type Props = {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `export default function \${1:${FILENAME_PASCALCASE}}({ \${2:children} }: Props) {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+  ],
+  description: 'React Functional Component',
+};
+
+exports.tsReactFunctionalExportComponentV2 = {
+  prefix: 'rfce $$rfce(ts)',
+  body: [
+    'type Props = {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `function \${1:${FILENAME_PASCALCASE}}({ \${2:children} }: Props) {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+    '',
+    'export default $1',
+  ],
+  description: 'React Functional Component',
+};
+
+exports.tsReactArrowFunctionComponentV2 = {
+  prefix: 'rafc $$rafc(ts)',
+  body: [
+    'type Props = {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `export const \${1:${FILENAME_PASCALCASE}} = ({ \${2:children} }: Props) => {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+  ],
+  description: 'React Arrow Function Component',
+};
+
+exports.tsReactArrowFunctionExportComponentV2 = {
+  prefix: 'rafce $$rafce(ts)',
+  body: [
+    'type Props = {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `const \${1:${FILENAME_PASCALCASE}} = ({ \${2:children} }: Props) => {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+    '',
+    'export default $1',
+  ],
+  description: 'React Arrow Function Component',
+};
