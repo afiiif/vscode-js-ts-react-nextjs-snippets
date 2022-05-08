e044453f9e62a26043e234951a43f6400294071e
M. Afifudin <afif.x.udin@gmail.com>
2022-05-08 10:42:45 +0700

    feat: add typescript snippets

--- src/snippets/react-hook-ts.js
+++ src/snippets/react-hook-ts.js
@@ -1,1 +1,25 @@
-module.exports = {};
+exports.tsUseState = {
+  prefix: '$$useStateSnippet',
+  body: [
+    'const [${1:state}, set${1/(.*)/${1:/capitalize}/}] = useState$0<${3:any}>(${2:initialValue})',
+  ],
+  description: 'React useState',
+};
+
+exports.tsUseRef = {
+  prefix: '$$useRefSnippet',
+  body: ['const ${1:ref} = useRef$0<${3:HTMLDivElement}>(${2:null})'],
+  description: 'React useRef',
+};
+
+exports.tsUseMemo = {
+  prefix: '$$useMemoSnippet',
+  body: ['useMemo<${3:any}>(() => ${1:computeValue}, [$2])'],
+  description: 'React useMemo',
+};
+
+exports.tsUseCallback = {
+  prefix: '$$useCallbackSnippet',
+  body: ['useCallback<() => ${3:void}>(() => {', '\t${1:// code}', '}, [$2])', ''],
+  description: 'React useCallback',
+};
--- src/snippets/react-ts.js
+++ src/snippets/react-ts.js
@@ -1,1 +1,69 @@
-module.exports = {};
+const { FILENAME_PASCALCASE } = require('../constants');
+
+exports.tsReactFunctionalComponent = {
+  prefix: '$$rfc',
+  body: [
+    'interface Props {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `export default function \${1:${FILENAME_PASCALCASE}}({ \${2:children} }: Props) {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+  ],
+  description: 'React Functional Component',
+};
+
+exports.tsReactFunctionalExportComponent = {
+  prefix: '$$rfce',
+  body: [
+    'interface Props {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `function \${1:${FILENAME_PASCALCASE}}({ \${2:children} }: Props) {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+    '',
+    'export default $1',
+  ],
+  description: 'React Functional Component',
+};
+
+exports.tsReactArrowFunctionComponent = {
+  prefix: '$$rafc',
+  body: [
+    'interface Props {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `export const \${1:${FILENAME_PASCALCASE}} = ({ \${2:children} }: Props) => {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+  ],
+  description: 'React Arrow Function Component',
+};
+
+exports.tsReactArrowFunctionExportComponent = {
+  prefix: '$$rafce',
+  body: [
+    'interface Props {',
+    '  $2: unknown;',
+    '}',
+    '',
+    `const \${1:${FILENAME_PASCALCASE}} = ({ \${2:children} }: Props) => {`,
+    '\treturn (',
+    '\t\t<${3:div}>${0:$1 Component}</$3>',
+    '\t)',
+    '}',
+    '',
+    'export default $1',
+  ],
+  description: 'React Arrow Function Component',
+};
