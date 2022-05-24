8f864d5b0bd1925503ba79d87949dd2ff4301512
M. Afifudin <afif.x.udin@gmail.com>
2022-05-24 17:36:36 +0700

    feat: add more wrapper snippets

--- src/snippets/wrapper.ts
+++ src/snippets/wrapper.ts
@@ -6,6 +6,24 @@ export const wrapWithFragment: Snippet = {
   description: 'Wrap component with Fragment',
 };
 
+export const wrapWithFunction: Snippet = {
+  prefix: ['_func', 'wrap'],
+  body: ['${1:func}(${TM_SELECTED_TEXT})'],
+  description: 'Wrap with function',
+};
+
+export const stringToTemplateLiterals: Snippet = {
+  prefix: ['_lit', 'wrap'],
+  body: ['`${TM_SELECTED_TEXT/^[\'"](.*)[\'"]$/$1/g}`'],
+  description: 'Convert regular string to template literals',
+};
+
+export const reactStringPropToTemplateLiteral: Snippet = {
+  prefix: ['_litr', 'wrap'],
+  body: ['{`${TM_SELECTED_TEXT/^[\'"](.*)[\'"]$/$1/g}`}'],
+  description: "Convert regular string in react prop's value to template literals",
+};
+
 export const wrapWithJsonStringify: Snippet = {
   prefix: ['_js', 'wrap'],
   body: ['JSON.stringify(${TM_SELECTED_TEXT})'],
