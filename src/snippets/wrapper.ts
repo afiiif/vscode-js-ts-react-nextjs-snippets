7a1d1fe2ea9593b529c06a28ff90acfd2d3b73c9
M. Afifudin <afif.x.udin@gmail.com>
2022-05-25 09:09:03 +0700

    feat: update prefix & clean-up `$TM_SELECTED_TEXT`

--- src/snippets/wrapper.ts
+++ src/snippets/wrapper.ts
@@ -2,31 +2,31 @@ import { Snippet } from '../types';
 
 export const wrapWithFunction: Snippet = {
   prefix: ['_func', 'wrap'],
-  body: ['${1:func}(${TM_SELECTED_TEXT})'],
+  body: ['${1:func}($TM_SELECTED_TEXT)'],
   description: 'Wrap with function',
 };
 
 export const stringToTemplateLiterals: Snippet = {
-  prefix: ['_lit', 'wrap'],
+  prefix: ['_lt', 'wrap'],
   body: ['`${TM_SELECTED_TEXT/^[\'"](.*)[\'"]$/$1/g}`'],
   description: 'Convert regular string to template literals',
 };
 
 export const reactStringPropToTemplateLiteral: Snippet = {
-  prefix: ['_litr', 'wrap'],
+  prefix: ['_ltr', 'wrap'],
   body: ['{`${TM_SELECTED_TEXT/^[\'"](.*)[\'"]$/$1/g}`}'],
   description: "Convert regular string in react prop's value to template literals",
 };
 
 export const wrapWithFragment: Snippet = {
   prefix: ['_frag', 'wrap'],
-  body: ['<$1$0>', '\t${TM_SELECTED_TEXT}', '</$1>'],
+  body: ['<$1$0>', '\t$TM_SELECTED_TEXT', '</$1>'],
   description: 'Wrap component with Fragment',
 };
 
 export const reactIf: Snippet = {
   prefix: ['_rif', 'wrap'],
-  body: ['{${1:condition} ? (', '\t${TM_SELECTED_TEXT}', ') : (', '\t${0:null}', ')}'],
+  body: ['{${1:condition} ? (', '\t$TM_SELECTED_TEXT', ') : (', '\t${0:null}', ')}'],
   description: 'Wrap in if',
 };
 
@@ -35,7 +35,7 @@ export const reactIfWithFragment: Snippet = {
   body: [
     '{${1:condition} ? (',
     '\t<>',
-    '\t\t${TM_SELECTED_TEXT}',
+    '\t\t$TM_SELECTED_TEXT',
     '\t</>',
     ') : (',
     '\t${0:null}',
@@ -46,7 +46,7 @@ export const reactIfWithFragment: Snippet = {
 
 export const reactElse: Snippet = {
   prefix: ['_rel', 'wrap'],
-  body: ['{${1:condition} ? (', '\t${0:null}', ') : (', '\t${TM_SELECTED_TEXT}', ')}'],
+  body: ['{${1:condition} ? (', '\t${0:null}', ') : (', '\t$TM_SELECTED_TEXT', ')}'],
   description: 'Wrap in else',
 };
 
@@ -57,7 +57,7 @@ export const reactElseWithFragment: Snippet = {
     '\t${0:null}',
     ') : (',
     '\t<>',
-    '\t\t${TM_SELECTED_TEXT}',
+    '\t\t$TM_SELECTED_TEXT',
     '\t</>',
     ')}',
   ],
@@ -66,24 +66,24 @@ export const reactElseWithFragment: Snippet = {
 
 export const reactConditional: Snippet = {
   prefix: ['_rcon', 'wrap'],
-  body: ['{${1:condition} && (', '\t${TM_SELECTED_TEXT}', ')}'],
+  body: ['{${1:condition} && (', '\t$TM_SELECTED_TEXT', ')}'],
   description: 'Wrap with conditional',
 };
 
 export const reactConditionalWithFragment: Snippet = {
   prefix: ['_rconf', 'wrap'],
-  body: ['{${1:condition} && (', '\t<>', '\t\t${TM_SELECTED_TEXT}', '\t</>', ')}'],
+  body: ['{${1:condition} && (', '\t<>', '\t\t$TM_SELECTED_TEXT', '\t</>', ')}'],
   description: 'Wrap with conditional',
 };
 
 export const wrapWithJsonStringify: Snippet = {
   prefix: ['_js', 'wrap'],
-  body: ['JSON.stringify(${TM_SELECTED_TEXT})'],
+  body: ['JSON.stringify($TM_SELECTED_TEXT)'],
   description: 'Wrap with `JSON.stringify`',
 };
 
 export const wrapWithJsonParse: Snippet = {
   prefix: ['_jp', 'wrap'],
-  body: ['JSON.parse(${TM_SELECTED_TEXT})'],
+  body: ['JSON.parse($TM_SELECTED_TEXT)'],
   description: 'Wrap with `JSON.parse`',
 };
