825b610ba2f9f64c202dcfeee19a5fa2a91d1efa
M. Afifudin <afif.x.udin@gmail.com>
2022-06-11 18:07:07 +0700

    feat: update testing snippets

--- src/snippets/testing.ts
+++ src/snippets/testing.ts
@@ -1,26 +1,32 @@
 import { FILENAME_PASCALCASE } from '../constants';
 import { Snippet } from '../types';
 
-export const tdesc: Snippet = {
-  prefix: ['tdescribe', 'describe(test)'],
+export const testDescribe: Snippet = {
+  prefix: ['tDescribe', 'describe(test)'],
   body: [`describe('\${1:${FILENAME_PASCALCASE}}', () => {`, '\t${0:// code}', '})'],
   description: 'Testing `describe` block',
 };
 
-export const tit: Snippet = {
-  prefix: ['tit', 'it(test)'],
+export const testIt: Snippet = {
+  prefix: ['tIt', 'it(test)'],
   body: ["it('should ${1:work}', ${2:async }() => {", '\t${0:// code}', '})'],
   description: 'Testing `it` block',
 };
 
-export const tbefore: Snippet = {
-  prefix: ['tbefore', 'before(test)'],
+export const test: Snippet = {
+  prefix: ['tTest', '(test)'],
+  body: [`test('\${1:${FILENAME_PASCALCASE}}', \${2:async }() => {`, '\t${0:// code}', '})'],
+  description: 'Testing `test` block',
+};
+
+export const testBefore: Snippet = {
+  prefix: ['tBefore', 'before(test)'],
   body: ['${1|beforeEach,beforeAll|}(() => {', '\t${0:// code}', '})'],
   description: 'Testing `beforeAll` / `beforeEach` block',
 };
 
-export const tafter: Snippet = {
-  prefix: ['tafter', 'after(test)'],
+export const testAfter: Snippet = {
+  prefix: ['tAfter', 'after(test)'],
   body: ['${1|afterEach,afterAll|}(() => {', '\t${0:// code}', '})'],
   description: 'Testing `afterAll` / `afterEach` block',
 };
