569c294fe198b773df856a1410f8f71b1675d448
M. Afifudin <afif.x.udin@gmail.com>
2022-07-19 09:44:57 +0700

    feat: add elm with `dangerouslySetInnerHTML` prop snippet

--- src/snippets/html-jsx.ts
+++ src/snippets/html-jsx.ts
@@ -1,6 +1,12 @@
 import { Snippet } from '../types';
 
-export const htmlButton: Snippet = {
+export const jsxInnerHTML: Snippet = {
+  prefix: ['innerHTML', 'dangerously'],
+  body: ['<${1:div} dangerouslySetInnerHTML={{ __html: $2 }} />'],
+  description: 'Create element with `dangerouslySetInnerHTML` prop',
+};
+
+export const jsxButton: Snippet = {
   prefix: 'button',
   body: ['<button type="button" onClick={${2:onClick}}$3>', '\t${1:Button}', '</button>'],
   description: 'Create `<button />` element',
